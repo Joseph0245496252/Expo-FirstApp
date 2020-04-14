3b48e977b47cfd466ebbcd3b8b8a31420af4d566
Joseph Asante <freecoding1@gmail.com>
2020-04-14 13:58:45 +0000

    my first mobile App

--- App.js
+++ App.js
@@ -2,36 +2,38 @@ import React from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native'
 import profile from './assets/profile.jpeg' 
 
-
-
 function App(){
   return <View style={styles.container}>
     <Image source={profile} style={styles.image}/>
    
     <View style={styles.detailContainer}>
-    <Text style={styles.lable}>Name</Text>
-    <Toxt style={[tyles.lable, styles.infor]}>Tony Start</Toxt>
+    <Text style={styles.label}>Name</Text>
+    <Text  style={[styles.label, styles.info]}>Tony Start</Text>
     </View>
 
     <View style={styles.detailContainer}>
-    <Text style={styles.lable}>Email</Text>
-    <Toxt style={[tyles.lable, styles.infor]}>freecoding1@gmail.com</Toxt>
+    <Text style={styles.label}>Gmail</Text>
+    <Text  style={[styles.label, styles.info]}>freecoding1@joseph.com</Text>
     </View>
 
     <View style={styles.detailContainer}>
-    <Text style={styles.lable}>Gender</Text>
-    <Toxt style={[tyles.lable, styles.infor]}>Male</Toxt>
+    <Text style={styles.label}>Gender</Text>
+    <Text  style={[styles.label, styles.info]}>Male</Text>
     </View>
+
   </View>
 
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
-    marginHorizontal: 5
+    flex:1,
+    marginHorizontal: 5,
+    backgroundColor: "#fbf" 
+
   },
-  imges:{
+
+  image: {
     marginTop: 100,
     width: 150,
     height: 150,
@@ -43,20 +45,23 @@ const styles = StyleSheet.create({
   detailContainer:{
     flexDirection: "row",
     marginVertical: 3
-  },
-  label:{
-    borderColor: "black",
+
+   },
+
+   label:{
+     color: 'blue',
+    borderColor: "orange",
     borderWidth: 1,
     fontSize: 20,
     flex: 2.5,
-    paddingVertical: 5
+    paddingHorizontal: 5
   },
-
-  infor: {
+  info: {
     flex: 7.5
   }
-
-
 });
 
-export default App;
+
+    export default App;
+
+  
